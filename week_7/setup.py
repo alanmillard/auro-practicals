0fed46e802f53e1007a58ae320cd08667577ba2b
Alan Millard <alan.millard@york.ac.uk>
2023-11-15 08:43:47 +0000

    Example code for Week 7 practical exercises

---
+++ week_7/setup.py
@@ -0,0 +1,53 @@
+# Based on https://answers.ros.org/question/397319/how-to-copy-folders-with-subfolders-to-package-installation-path/
+
+import os
+from setuptools import find_packages, setup
+
+package_name = 'week_7'
+
+data_files=[
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+]
+
+def package_files(data_files, directory_list):
+
+    paths_dict = {}
+
+    for directory in directory_list:
+
+        for (path, directories, filenames) in os.walk(directory):
+
+            for filename in filenames:
+
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+                else:
+                    paths_dict[install_path] = [file_path]
+
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+
+    return data_files
+
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=package_files(data_files, ['models/', 'launch/', 'worlds/', 'rviz/']),
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='am567',
+    maintainer_email='alan.millard@york.ac.uk',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
